f7970dcd48549d99a6f583f950c8f5def9b68155
Mubeen142 <mubeenullah323@gmail.com>
2025-01-29 14:04:06 +0200

    init

---
+++ src/Providers/LaraPayServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LaraPay\Foundation\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraPayServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
