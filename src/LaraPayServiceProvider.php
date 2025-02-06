1b1fd74f3fe4db2af169660270a49c867a63b297
Mubeen142 <mubeenullah323@gmail.com>
2025-02-06 19:09:42 +0200

    Update LaraPayServiceProvider.php

--- src/LaraPayServiceProvider.php
+++ src/LaraPayServiceProvider.php
@@ -13,10 +13,10 @@ class LaraPayServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../../assets/2025_1_29_000000_create_larapay_tables.php' => database_path('migrations/2025_1_29_000000_create_larapay_tables.php'),
-            __DIR__.'/../../assets/Gateway.php' => app_path('Models/Gateway.php'),
-            __DIR__.'/../../assets/Payment.php' => app_path('Models/Payment.php'),
-            __DIR__.'/../../assets/Subscription.php' => app_path('Models/Subscription.php'),
+            __DIR__.'/../assets/Gateway.php' => app_path('Models/Gateway.php'),
+            __DIR__.'/../assets/Payment.php' => app_path('Models/Payment.php'),
+            __DIR__.'/../assets/Subscription.php' => app_path('Models/Subscription.php'),
+            __DIR__.'/../assets/2025_1_29_000000_create_larapay_tables.php' => database_path('migrations/2025_1_29_000000_create_larapay_tables.php'),
         ], 'larapay');
 
         $this->commands([
@@ -29,8 +29,6 @@ class LaraPayServiceProvider extends ServiceProvider
         // load the config file
         $this->mergeConfigFrom(__DIR__ . '/Config/larapay.php', 'larapay');
 
-        // load the migrations
-        $this->loadMigrationsFrom(__DIR__ . '/Migrations');
 
         // load the routes
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
